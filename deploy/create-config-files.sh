1152873365d8c5709878750f6848d1291324f025
Talen Fisher <talen.fisher@cythral.com>
2020-04-16 09:19:33 -0500

    BDC-2 Tag Resources (#1)
    Tags resources for bdc-online in AWS.

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for file in $(find deploy/params -name '*.json'); do
+dir=$(dirname "${BASH_SOURCE[0]}");
+
+for file in $(find $dir/params -name '*.json'); do
     envName=$(echo $file | xargs basename | sed 's/\.json//g')
     params=$(cat $file)
 
-    config=$(echo {} | jq --argjson params "$params" '.Parameters=$params')
+    config=$(cat $dir/config.json | jq --argjson params "$params" '.Parameters=$params')
     echo $config > bdc-online.${envName}.config.json
 done
+
+cp $dir/config.json dns.config.json
